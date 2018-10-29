88f01837fc07a3f1ae5cef867be7684cb97f2bfa
d.huhushkin <holderok@gmail.com>
2018-10-29 14:52:27 +0200

    eee

---
+++ src/main/java/ua/gov/nais/pages/MotherPage.java
@@ -0,0 +1,20 @@
+package ua.gov.nais.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.sql.Driver;
+
+public abstract class MotherPage {
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+    public MotherPage(WebDriver driver){
+        this.driver = driver;
+        wait = new WebDriverWait(driver, 30);
+        PageFactory.initElements(driver, this);
+    }
+
+}
