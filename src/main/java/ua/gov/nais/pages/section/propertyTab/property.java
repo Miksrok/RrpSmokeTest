7c810079554eb711caa5e8ea1e19d482bf3d339c
abrusovv <abrusovv@gmail.com>
2018-11-02 14:40:41 +0200

    Added new packages and pages. section and property

---
+++ src/main/java/ua/gov/nais/pages/section/propertyTab/property.java
@@ -0,0 +1,15 @@
+package ua.gov.nais.pages.section.propertyTab;
+
+import org.openqa.selenium.WebDriver;
+import ua.gov.nais.pages.MotherPage;
+
+public class property extends MotherPage {
+
+
+
+
+
+
+
+    public property(WebDriver driver) {super(driver); }
+}
